e0d23ea75b1b14d1c56e0f310e59cd8ac53ba1dd
swarnaka-liyanaarachchi <swarnaka@mbizsoftware.com>
2024-06-05 00:22:40 +0530

    hompage4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemListComponent } from './item-list/item-list.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { CreateItemComponent } from './create-item/create-item.component';
 
 import { FormsModule } from '@angular/forms';
